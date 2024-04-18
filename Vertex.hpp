1d23040d0b9e5f9d3c9f6fa8d490d1811607c521
Thao2004 <hoangmai@usf.edu>
2024-04-18 17:44:49 -0400

    Update Vertex.hpp

--- Vertex.hpp
+++ Vertex.hpp
@@ -7,13 +7,14 @@
 class Vertex {
 public:
     Vertex(std::string label) : label(label) {}
-    void addEdge(Vertex* vertex, unsigned long weight);
-    void removeEdge(Vertex* vertex);
-    const std::string& getLabel() const;
+    void addEdge(Vertex* vertex, unsigned long weight) { edges[vertex] = weight; }
+    void removeEdge(Vertex* vertex) { edges.erase(vertex); }
+    const std::string& getLabel() const { return label; }
+    const std::unordered_map<Vertex*, unsigned long>& getEdges() const { return edges; }
 
 private:
     std::string label;
+    std::unordered_map<Vertex*, unsigned long> edges;
 };
-
 
 #endif
