085582fe997e602a123503fe1606b86082691677
Nguyen Hoang Mai Thao <thaonguyen@eduroam-226-86-90.laptops.usf.edu>
2024-04-18 17:26:30 -0400

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -1,4 +1,21 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include "GraphBase.hpp"
+
+class Graph : GraphBase {
+public:
+    Graph() {}
+    ~Graph();
+
+    virtual void addVertex(std::string label) override;
+    virtual void removeVertex(std::string label) override;
+    virtual void addEdge(std::string label1, std::string label2, unsigned long weight) override;
+    virtual void removeEdge(std::string label1, std::string label2) override;
+    virtual unsigned long shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path) override;
+
+    void printGraph();
+
+};
+
 #endif
