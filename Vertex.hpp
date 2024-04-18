43f2a4170b1b2367a1ac27bf253edfccfe429363
Thao2004 <hoangmai@usf.edu>
2024-04-18 17:37:50 -0400

    Vertex Interface

--- Vertex.hpp
+++ Vertex.hpp
@@ -1,7 +1,19 @@
 #ifndef VERTEX_H
 #define VERTEX_H
 
+#include <string>
+#include <unordered_map>
 
+class Vertex {
+public:
+    Vertex(std::string label) : label(label) {}
+    void addEdge(Vertex* vertex, unsigned long weight);
+    void removeEdge(Vertex* vertex);
+    const std::string& getLabel() const;
+
+private:
+    std::string label;
+};
 
 
 #endif
