282e3a5a7358979e723aa7d32079d6e98531675b
Thao2004 <hoangmai@usf.edu>
2024-04-18 17:40:49 -0400

    Edge Interface

--- Edge.hpp
+++ Edge.hpp
@@ -1,6 +1,17 @@
 #ifndef EDGE_H
 #define EDGE_H
 
+#include "Vertex.hpp" 
+
+class Edge {
+public: 
+    Edge(Vertex* v1, Vertex* v2, unsigned long weight);
+
+private:
+    Vertex* vertex1;
+    Vertex* vertex2;
+    unsigned long weight;
+};
 
 
 #endif
